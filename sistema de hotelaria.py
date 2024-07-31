714d01797950d4308aa5b8520955b4c2019fc82a
diego <141512965+klawwwz@users.noreply.github.com>
2024-07-31 11:12:55 -0300

    sistema de hotelaria.py
    sistema de hotelaria com informações adicionais criado na sala

---
+++ sistema de hotelaria.py
@@ -0,0 +1,50 @@
+#Você é responsavel por criar um sistema simples para gerenciar as reservas de quartos de um hotel.
+#o hospede precisa entrar com o nome, check-in, check-out e numero do quarto
+
+
+def adc_reserva(nome, checkin, checkout, numero):
+    #aqui precisa adicionar os nomes e quartos ocupados numa lista
+    pass
+
+def cncl_reserva(hotel, hospede):
+    pass
+
+def bsc_reserva():
+    pass
+
+def lst_reserva():
+    pass
+
+def main():
+    while True:
+        ('Bem-vindo ao Hazbin Hotel!')
+        ('[1]-Criar reserva de cliente\n[2]-Remover reserva\n[3]-Buscar reserva\n[4]-Listar todas as reservas')
+        esc = input('')
+
+        if esc == '1':
+            ('Adicionar reserva:\nTenha em mãos seus documentos!')
+            nome = input('Digite seu nome completo: ')
+            checkin = input('Data do check-in: ')
+            checkout = input('Data de saída: ')
+            #passar uma lista com os quartos vazios para o hospede escolher(mas posso colocar dentro da função adc)
+
+            pass
+
+        if esc == '2':
+            ('Remover reserva: ')
+            #adicionar um identificador em cada reserva, na hora de remover so preicsa remover o identificador da lista
+            #e transformar o qaurto ocupado em vazio
+            pass
+
+        if esc == '3':
+            
+            pass
+
+        if esc == '4':
+            pass
+
+
+
+if __name__ == "__main__":
+    main()
+    pass

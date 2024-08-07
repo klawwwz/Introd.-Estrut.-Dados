4b490e17ec3fbe35a7444bb2ab4177fb38e8c99c
diego <141512965+klawwwz@users.noreply.github.com>
2024-08-07 05:22:08 -0300

    sistema de hotelaria.py

--- sistema de hotelaria.py
+++ sistema de hotelaria.py
@@ -1,29 +1,47 @@
 #Você é responsavel por criar um sistema simples para gerenciar as reservas de quartos de um hotel.
 #o hospede precisa entrar com o nome, check-in, check-out e numero do quarto
 
-
-def adc_reserva(reservas, id, nome, checkin, checkout, numero):
-    #aqui precisa adicionar os nomes e quartos ocupados numa lista
+def adc_reserva(reservas, numero, id_ant, nome, checkin_dia, checkin_mes, checkout_dia, checkout_mes):
+    id_reserva = id_ant + 1
     reserva = {
-        'id' : id, #o id nao é o usuario que coloca
+        'id' : id_reserva, 
         'nome' : nome,
         'numero do quarto' : numero,
-        'checkin' : checkin,
-        'checkout' : checkout
+        'checkin' : {
+        'dia do checkin' : checkin_dia,
+        'mes do checkin' : checkin_mes
+        },
+        'checkout' : {
+        'dia do checkout' : checkout_dia,
+        'mes do checkout' : checkout_mes
+        }
     }
     reservas.append(reserva)
-    pass
+    print(f'Reserva adicionada com sucesso! Seu ID é {id_reserva}')
+    return  id_reserva 
 
-def cncl_reserva(hotel, hospede):
-    pass
+def cncl_reserva(reservas, id_reserva):
+    for reserva in reservas:
+        if reserva['id'] == id_reserva:
+            reservas.remove(reserva)
+            print('Reserva cancelada com sucesso!')
+        else:
+            print('Reserva não foi encontrada.')
 
-def bsc_reserva():
-    pass
+
+def bsc_reserva(reservas, nome):
+    a = False
+    for reserva in reservas:
+        if reserva['nome'] == nome:
+            print(f"ID: {reserva['id']}, Nome: {reserva['nome']}, Quarto: {reserva['numero do quarto']}, Check-in: {reserva['checkin']}, Check-out: {reserva['checkout']}")
+            a = True
+    if not a:
+        print('Reserva não encontrada.')
 
 def menu_edt():
-    ('O que você deseja editar?')
-    ('[1]-Nome\n[2]-Numero do quarto\n[3]-Data do check in\n[4]-Data do check out.')
-    esc = input('')
+    print('O que você deseja editar?')
+    print('[1]-Nome\n[2]-Numero do quarto\n[3]-Dia do check in\n[4]-Mês do checkin\n[5]-Dia do checkout\n[6]-Mês do checkout.')
+    esc = int(input(''))
     return esc
 
 
@@ -32,48 +50,79 @@ def edt_reserva(reservas, id_reserva):
         for reserva in reservas:
             if reserva['id'] == id_reserva:
                 opcao = menu_edt()
-                if opcao == '1':
+                if opcao == 1:
                     nome = input('Digite o novo nome: ')
                     reserva['nome'] = nome
-                elif opcao == '2':
+                    print(f'nome alterado com sucesso.')
+                elif opcao == 2:
                     num = input('Digite o novo numero do qaurto: ')
                     reserva['numero do quarto'] = num
-                elif opcao == '3':
-                    checkin = input('Atualize a data do checkin: ')
-                    reserva['checkin'] = checkin
-                elif opcao == '4':
-                    checkout = input('Digite a data do checkout: ')
-                    reserva['checkout'] = checkout 
+                    print(f'numero alterado com sucesso.')
+                elif opcao == 3:
+                    dia_checkin = input('Atualize a data do dia do checkin: ')
+                    reserva['dia do checkin'] = dia_checkin
+                    print(f'dia alterado com sucesso.')
+                elif opcao == 4:
+                    checkin_mes = input('Atualize a data do mês do checkin: ')
+                    reserva['mes do checkin'] = checkin_mes 
+                    print(f'mês alterado com sucesso.')
+                elif opcao == 5:
+                    checkout_dia = input('Atualize o dia do checkout: ')
+                    reserva['dia do checkout'] = checkout_dia 
+                    print(f'dia alterado com sucesso.')
+                elif opcao == 6:
+                    checkout_mes = input('Atualize o mês do checkout: ')
+                    reserva['mes do checkout'] = checkout_mes
+                    print(f'mês alterado com sucesso.')
 
-def lst_reserva():
+                    pass
+
+def lst_reserva(reservas):
+     if not reservas:
+        print('Não há reservas na lista')
+        for reserva in reservas:
+            print(f"ID: {reserva['id']}, Nome: {reserva['nome']}, Quarto: {reserva['numero do quarto']}, Check-in: {reserva['dia do checkin']} / {reserva['mes do checkin']}, Check-out: {reserva['dia do checkout']} / {reserva['mes do checkout']}")
+
+def chk_disponivel():
     pass
 
 def main():
+    id_ant = 0
+    reservas = []
     while True:
-        ('Bem-vindo ao Hazbin Hotel!')
-        ('[1]-Criar reserva de cliente\n[2]-Remover reserva\n[3]-Buscar reserva\n[4]-Listar todas as reservas')
-        esc = input('')
+        print('Bem-vindo ao Hazbin Hotel!')
+        print('[1]-Criar reserva\n[2]-Remover reserva\n[3]-Buscar reserva\n[4]-Listar todas as reservas\n[5]-Editar reserva')
+        esc = int(input(''))
 
-        if esc == '1':
-            ('Adicionar reserva:\nTenha em mãos seus documentos!')
+        if esc == 1:
+            print('Adicionar reserva:\nTenha em mãos seus documentos!')
             nome = input('Digite seu nome completo: ')
-            checkin = input('Data do check-in: ')
-            checkout = input('Data de saída: ')
-            #passar uma lista com os quartos vazios para o hospede escolher(mas posso colocar dentro da função adc)
+            checkin_dia = int(input('Dia do check-in: '))
+            checkin_mes = int(input('Mês do check-in: '))
+            checkout_dia = int(input('Dia de saída: '))
+            checkout_mes = int(input('Mês de saída: '))
+            numero = int(input('Numero do quarto desejado: '))
+            chk_disponivel()
+            id_ant = adc_reserva(reservas, numero, id_ant, nome, checkin_dia, checkin_mes, checkout_dia, checkout_mes)
 
-            pass
+        elif esc == 2:
+            print('Remover reserva: ')
+            id = int(input('digite o id que deseja cancelar: '))
+            cncl_reserva(reservas, id)
 
-        elif esc == '2':
-            ('Remover reserva: ')
-            #adicionar um identificador em cada reserva, na hora de remover so preicsa remover o identificador da lista
 
-        elif esc == '3':
-            
-            pass
+        elif esc == 3:
+            print('Buscar reserva: ')
+            nome = input('Deseja buscar reservas no nome de quem? ')
+            bsc_reserva(reservas, nome)
 
-        elif esc == '4':
-            pass
-
+        elif esc == 4:
+            print('Listar reservas: ')
+            lst_reserva(reservas)
+        elif esc == 5:
+            print('EDITANDO RESERVAS: ')
+            id = int(input('digite o id cujo você deseja editar as informações: '))
+            edt_reserva(reservas, id)
 if __name__ == "__main__":
     main()
     pass
